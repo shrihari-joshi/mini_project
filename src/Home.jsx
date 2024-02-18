bc338a6699fcbc9b2502e5199ecacbb435e6a4ad
shrihari-joshi <joshishrihari13@gmail.com>
2024-02-18 13:59:51 +0530

    right nav-bar

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,30 +1,53 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from './Navbar';
 import './navbar.css';
+
+
+import { FaCartPlus } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
+import { PiSignInBold } from "react-icons/pi";
+import { IoSettingsSharp } from "react-icons/io5";
+
 const Home = () => {
-    
+    const [isSlidingBarOpen, setSlidingBarOpen] = useState(false);
+    const slidingBarRef = useRef(null);
+
+    const toggleSlidingBar = () => {
+        setSlidingBarOpen(true);
+    };
+
+    const closeSlidingBar = () => {
+        setSlidingBarOpen(false);
+    };
 
     return (
         <div className='container'>
+            <div className={`left ${isSlidingBarOpen ? 'sliding-bar-open' : ''}`}>
+                <Navbar />
+            </div>
 
-                <div className='left'>
-                    <Navbar/>
+            <div className='right body'>
+                <div className="toggle-button" onClick={toggleSlidingBar}>
+                    ☰
                 </div>
-                
-                <div className='right body'>
-                    <div>
-                        <p className='title'>Kissan Utsav</p>
-                        <p className='h1'>Cultivating Connection, Connecting Lives!</p> 
-                    </div>
-                        
-                    <div className='image'>
-                        <p>
-                            Ithe Image yeil
-                        </p>
-                    </div>
 
+                <div>
+                    <p className='title'>Kissan Utsav</p>
+                    <p className='h1'>Seeds of Change!</p>
                 </div>
+
+                <div
+                    ref={slidingBarRef}
+                    className={`sliding-bar ${isSlidingBarOpen ? 'open' : ''}`}
+                    onMouseLeave={closeSlidingBar}
+                >
+                    <div className="tab" id="cart">{<FaCartPlus />}</div>
+                    <div className="tab" id="wishlist">{<FaHeart />}</div>
+                    <div className="tab" id="settings">{<IoSettingsSharp />}</div>
+                    <div className="tab" id="sign-in">{<PiSignInBold />}</div>
+                </div>
+            </div>
         </div>
     );
 };
